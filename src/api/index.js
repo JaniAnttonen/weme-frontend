41342174f529e22a7c802b39c7f0a82b3cfeafe3
Jani Anttonen <jantto@utu.fi>
2019-03-08 00:23:29 +0200

    add some boilerplate for API

---
+++ src/api/index.js
@@ -0,0 +1,3 @@
+export const fetchGoogle = async body => {
+  return await fetch('127.0.0.1')
+}
--- src/pages/app/login.js
+++ src/pages/app/login.js
@@ -1,38 +1,52 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'gatsby'
+
+import * as api from '../../api'
 
 import Layout from '../../components/layout'
 import SEO from '../../components/seo'
 import Main from '../../components/main'
 
-const Login = () => (
-  <Layout>
-    <SEO title="Sign In" keywords={[`weme`, `login`]} />
-    <Main className="py-10">
-      <div className="container flex flex-col items-center justify-center">
-        <h1 className="font-heading">Sign in</h1>
-        <div className="flex flex-col items-center md:w-1/3 sm:w-1/2 mt-10">
-          <input
-            type="text"
-            className="mb-4 p-2 shadow-inner rounded-input w-full border border-grey-dark"
-            placeholder="Username*"
-          />
-          <input
-            type="text"
-            className="mb-4 p-2 shadow-inner rounded-input w-full border border-grey-dark"
-            placeholder="Password*"
-          />
-          <a>Forgot password?</a>
-          <button className="bg-teal hover:bg-green hover:border-green text-white py-2 px-10 border border-teal rounded no-underline mt-10 mb-2 shadow">
-            Login
-          </button>
-          <div>
-            Do not have an account? Please, <Link to="/app/join">sign up</Link>
+const Login = () => {
+  const [state, setState] = useState({ username: '', password: '' })
+  const onSubmit = () => api.fetchGoogle().then(console.log)
+  return (
+    <Layout>
+      <SEO title="Sign In" keywords={[`weme`, `login`]} />
+      <Main className="py-10">
+        <div className="container flex flex-col items-center justify-center">
+          <h1 className="font-heading">Sign in</h1>
+          <div className="flex flex-col items-center md:w-1/3 sm:w-1/2 mt-10">
+            <input
+              type="text"
+              className="mb-4 p-2 shadow-inner rounded-input w-full border border-grey-dark"
+              placeholder="Username*"
+              value={state.username}
+              onChange={e => setState({ username: e.target.value })}
+            />
+            <input
+              type="text"
+              className="mb-4 p-2 shadow-inner rounded-input w-full border border-grey-dark"
+              placeholder="Password*"
+              value={state.password}
+              onChange={e => setState({ password: e.target.value })}
+            />
+            <a>Forgot password?</a>
+            <button
+              className="bg-teal hover:bg-green hover:border-green text-white py-2 px-10 border border-teal rounded no-underline mt-10 mb-2 shadow"
+              onClick={() => onSubmit()}
+            >
+              Login
+            </button>
+            <div>
+              Do not have an account? Please,{' '}
+              <Link to="/app/join">sign up</Link>
+            </div>
           </div>
         </div>
-      </div>
-    </Main>
-  </Layout>
-)
+      </Main>
+    </Layout>
+  )
+}
 
 export default Login
