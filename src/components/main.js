2261fc27f65f1dc97ac81f8455386b412c425316
Jani Anttonen <jantto@utu.fi>
2019-03-08 00:26:45 +0200

    use semantic html5

--- src/components/main.js
+++ src/components/main.js
@@ -3,13 +3,13 @@ import React from 'react'
 const Main = props => {
   const { children, className } = props
   return (
-    <div
+    <main
       className={`container mx-auto w-full max-w-xl px-2 flex flex-wrap justify-stretch${
         className ? ' ' + className : ''
       }`}
     >
       {children}
-    </div>
+    </main>
   )
 }
 
